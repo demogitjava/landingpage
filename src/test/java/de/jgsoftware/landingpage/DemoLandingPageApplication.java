a6f61bc9ece8385c7de47d3c6f80948dd5846807
hoscho <jgeist@gmx.net>
2022-02-28 06:55:07 +0100

    .

---
+++ src/test/java/de/jgsoftware/landingpage/DemoLandingPageApplication.java
@@ -0,0 +1,13 @@
+package de.jgsoftware.landingpage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLandingPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoLandingPageApplication.class, args);
+    }
+
+}
+
