509923a8b6cf362d3d7588fb805da7425f448cf5
hoscho <jgeist@gmx.net>
2022-03-04 11:49:58 +0100

    add userlogin form to LoginController

--- src/main/java/de/jgsoftware/landingpage/controller/LoginController.java
+++ src/main/java/de/jgsoftware/landingpage/controller/LoginController.java
@@ -3,6 +3,7 @@ package de.jgsoftware.landingpage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.support.RequestContextUtils;
@@ -14,11 +15,19 @@ public class LoginController
 
     // load default landingpage
     @GetMapping({"login", "/"})
-    public ModelAndView demo() {
+    public ModelAndView login() {
 
         ModelAndView mv = new ModelAndView("login");
         
         return mv;
+    }
+
+    @PostMapping("/userdata")
+    public String login(String username, String password)
+    {
+
+
+        return "login";
     }
 
 }
