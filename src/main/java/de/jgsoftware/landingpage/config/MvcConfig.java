3948372f864ec10ff1cda42bf20c90c92c3c5446
hoscho <jgeist@gmx.net>
2022-03-12 15:06:51 +0100

    eidt mvc config and image 900x400.png

--- src/main/java/de/jgsoftware/landingpage/config/MvcConfig.java
+++ src/main/java/de/jgsoftware/landingpage/config/MvcConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import de.jgsoftware.landingpage.config.AppConfigLocale;
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer
 {
