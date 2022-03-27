9222d936e119b979568ebd77944e14ada2843497
hoscho <jgeist@gmx.net>
2022-03-27 09:53:10 +0200

    load bootstrap comp and bootstrap text to landingpage controller of english

--- src/main/java/de/jgsoftware/landingpage/controller/EN_LandingPageController.java
+++ src/main/java/de/jgsoftware/landingpage/controller/EN_LandingPageController.java
@@ -2,14 +2,22 @@ package de.jgsoftware.landingpage.controller;
 
 
 import de.jgsoftware.landingpage.controller.interfaces.iEN_Controller;
+import de.jgsoftware.landingpage.service.interfaces.i_service_en;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 public class EN_LandingPageController implements iEN_Controller
 {
+    ModelAndView mv;
+
+    @Autowired
+    i_service_en i_service_en;
+
 
     // load default landingpage
     @Override
@@ -17,7 +25,11 @@ public class EN_LandingPageController implements iEN_Controller
 
         //model.addAttribute("lgname", plgservice.getMultipagelanguage().getPageLanguageText());
         //model.addAttribute("productList", indexservice.getDaoProduct().getProductsforLandingpage());
-        ModelAndView mv = new ModelAndView("en");
+        mv = new ModelAndView("en");
+
+        // load all bootstrap components to MVC Controller
+        mv.addObject("webtextcomp", i_service_en.getiDaoEn().getPageLanguageText());
+        mv.addObject("btcomp", i_service_en.getiDaoEn().getBootstrapComponents());
 
 
         return mv;
