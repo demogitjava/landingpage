372d79fe9f8ac334eeeaa506153d910175aafe1d
hoscho <jgeist@gmx.net>
2022-03-31 09:06:00 +0200

    edit pom.xml

--- src/main/java/de/jgsoftware/landingpage/iDemoLandingPage.java
+++ src/main/java/de/jgsoftware/landingpage/iDemoLandingPage.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public interface iDemoLandingPage
 {
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(DemoLandingPageApplication.class, args);
     }
