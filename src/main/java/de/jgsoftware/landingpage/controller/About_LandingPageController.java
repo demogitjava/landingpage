46d5eeccf75424282e94208acc32eeb6c5fc4127
hoscho <jgeist@gmx.net>
2022-04-10 13:13:11 +0200

    edit de_header.html for display language in german

--- src/main/java/de/jgsoftware/landingpage/controller/About_LandingPageController.java
+++ src/main/java/de/jgsoftware/landingpage/controller/About_LandingPageController.java
@@ -2,14 +2,28 @@ package de.jgsoftware.landingpage.controller;
 
 
 import de.jgsoftware.landingpage.controller.interfaces.iAboutController;
+import de.jgsoftware.landingpage.service.interfaces.IServiceIndex;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class About_LandingPageController implements iAboutController
 {
 
+
+    java.util.Locale locale;
+
+    @Autowired
+    IServiceIndex iServiceIndex;
+
+    @Autowired
+    HttpServletRequest request;
+
     ModelAndView mv;
+
 
 
     @Override
@@ -21,6 +35,16 @@ public class About_LandingPageController implements iAboutController
 
 
 
+        /**
+         *   load entities from table
+         *   webtextlayout to contoller
+         *
+         *   inject text from with webtextcomp[0] fist id of table
+         */
+        mv.addObject("webtextcomp", iServiceIndex.getDcontroller().getPageLanguageText());
+
+
+        mv.addObject("btcomp", iServiceIndex.getDcontroller().getBootstrapComponents());
 
 
         return mv;
