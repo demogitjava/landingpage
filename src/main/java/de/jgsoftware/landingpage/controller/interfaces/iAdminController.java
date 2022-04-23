daeb1aca50273d336f5599e3e7db0d37aa0ba5b8
hoscho <jgeist@gmx.net>
2022-04-23 19:29:58 +0200

    .

--- src/main/java/de/jgsoftware/landingpage/controller/interfaces/iAdminController.java
+++ src/main/java/de/jgsoftware/landingpage/controller/interfaces/iAdminController.java
@@ -1,6 +1,5 @@
 package de.jgsoftware.landingpage.controller.interfaces;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
