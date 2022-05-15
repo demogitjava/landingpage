96be2837f0d9fbdb6f8a3172d8d8975ba2d06266
hoscho <jgeist@gmx.net>
2022-05-15 17:22:34 +0200

    .

--- src/main/java/de/jgsoftware/landingpage/controller/Admin_Controller.java
+++ src/main/java/de/jgsoftware/landingpage/controller/Admin_Controller.java
@@ -2,6 +2,8 @@ package de.jgsoftware.landingpage.controller;
 
 
 import de.jgsoftware.landingpage.controller.interfaces.iAdminController;
+import de.jgsoftware.landingpage.service.interfaces.IServiceIndex;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 
 @Controller
@@ -19,10 +23,23 @@ public class Admin_Controller implements iAdminController
 
     ModelAndView mv;
 
+    java.util.Locale locale;
+    @Autowired
+    IServiceIndex iServiceIndex;
+    @Autowired
+    HttpServletRequest request;
+
+
+    Principal principal;
+
     @Override
     public ModelAndView admincontroller()
     {
-        mv = new ModelAndView("admin");
+
+
+
+
+
         return mv;
     }
 
--- src/main/java/de/jgsoftware/landingpage/controller/interfaces/iAdminController.java
+++ src/main/java/de/jgsoftware/landingpage/controller/interfaces/iAdminController.java
@@ -1,5 +1,6 @@
 package de.jgsoftware.landingpage.controller.interfaces;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 
 @RequestMapping("/admin")
+@PreAuthorize("hasAuthority('ROLE_ADMIN','ROLE_USER')")
 public interface iAdminController
 {
     @GetMapping({"admin", "/"})
