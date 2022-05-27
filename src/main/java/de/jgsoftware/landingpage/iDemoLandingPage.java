01d39d59bc58d8ea3c8b213831f3dc2230e1ccee
hoscho <jgeist@gmx.net>
2022-05-27 08:12:43 +0200

    edit pom setup for cloud server

--- src/main/java/de/jgsoftware/landingpage/iDemoLandingPage.java
+++ src/main/java/de/jgsoftware/landingpage/iDemoLandingPage.java
@@ -3,9 +3,11 @@ package de.jgsoftware.landingpage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
+@EnableConfigServer
 public interface iDemoLandingPage
 {
     static void main(String[] args)
