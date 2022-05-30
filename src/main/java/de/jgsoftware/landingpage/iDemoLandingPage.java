65d7aae3db95a2f1568e3ae1998eb34f67732606
hoscho <jgeist@gmx.net>
2022-05-30 09:30:17 +0200

    edit pom and delte setup for cloud spring boot server

--- src/main/java/de/jgsoftware/landingpage/iDemoLandingPage.java
+++ src/main/java/de/jgsoftware/landingpage/iDemoLandingPage.java
@@ -3,11 +3,11 @@ package de.jgsoftware.landingpage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+//import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
-@EnableConfigServer
+//@EnableConfigServer
 public interface iDemoLandingPage
 {
     static void main(String[] args)
