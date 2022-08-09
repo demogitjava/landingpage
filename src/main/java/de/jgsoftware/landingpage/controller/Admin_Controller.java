6c4ea944c33524186b816c316e012fee825a50f4
hoscho <jgeist@gmx.net>
2022-08-09 16:15:00 +0200

    h2-console is now available over html interface

--- src/main/java/de/jgsoftware/landingpage/controller/Admin_Controller.java
+++ src/main/java/de/jgsoftware/landingpage/controller/Admin_Controller.java
@@ -3,15 +3,9 @@ package de.jgsoftware.landingpage.controller;
 
 import de.jgsoftware.landingpage.controller.interfaces.iAdminController;
 import de.jgsoftware.landingpage.model.m_webtextlayout;
-import de.jgsoftware.landingpage.service.interfaces.IServiceIndex;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
@@ -40,7 +34,7 @@ public class Admin_Controller implements iAdminController
     Principal principal;
 
     @Override
-    public ModelAndView admincontroller()
+    public ModelAndView admin()
     {
 
         /**
--- src/main/java/de/jgsoftware/landingpage/controller/interfaces/iAdminController.java
+++ src/main/java/de/jgsoftware/landingpage/controller/interfaces/iAdminController.java
@@ -8,13 +8,20 @@ import org.springframework.web.servlet.ModelAndView;
 
 import de.jgsoftware.landingpage.model.m_webtextlayout;
 
-@RequestMapping("admin")
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequestMapping("/admin")
 @PreAuthorize("hasAuthority('ROLE_ADMIN','ROLE_USER')")
 public interface iAdminController
 {
-    @GetMapping({"admin", "/"})
-    ModelAndView admincontroller();
 
+    @GetMapping({"admin", "/"})
+    ModelAndView admin();
 
     @PostMapping("/edit/crudlist")
     public String submitForm(m_webtextlayout webtextlayout);
