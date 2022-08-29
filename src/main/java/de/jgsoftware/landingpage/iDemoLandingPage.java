eb1ad56b83f38841418aa8b3bc5cc0e8f838de3b
hoscho <jgeist@gmx.net>
2022-08-29 11:13:26 +0200

    add timezone to maven pom

--- src/main/java/de/jgsoftware/landingpage/iDemoLandingPage.java
+++ src/main/java/de/jgsoftware/landingpage/iDemoLandingPage.java
@@ -3,6 +3,8 @@ package de.jgsoftware.landingpage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 //import org.springframework.cloud.config.server.EnableConfigServer;
 
 
@@ -12,6 +14,7 @@ public interface iDemoLandingPage
 {
     static void main(String[] args)
     {
+
         SpringApplication.run(DemoLandingPageApplication.class, args);
     }
 }
