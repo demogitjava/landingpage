a05f97ab82c5e01a27943d4a2ab4f27922f0fe90
hoscho <jgeist@gmx.net>
2022-09-01 18:29:02 +0200

    edit wiki.html for requested language

--- src/main/java/de/jgsoftware/landingpage/controller/de_wiki.java
+++ src/main/java/de/jgsoftware/landingpage/controller/de_wiki.java
@@ -17,8 +17,7 @@ public class de_wiki implements i_de_wiki
 
     java.util.Locale locale;
 
-    @Autowired
-    IServiceIndex iServiceIndex;
+
     @Autowired
     HttpServletRequest request;
 
@@ -35,23 +34,13 @@ public class de_wiki implements i_de_wiki
         mv = new ModelAndView("wiki");
 
 
-
-        /**
-         *   Get Country to display Language
-         *   only for this Controller
+        /*
+                language from useragent
          */
         String languagestr = RequestContextUtils.getLocale(request).getLanguage();
+
         mv.addObject("lang", languagestr);
 
-
-        /**
-         *   load entities from table
-         *   webtextlayout to contoller
-         *
-         *   inject text from with webtextcomp[0] fist id of table
-         */
-        mv.addObject("webtextcomp", iServiceIndex.getDcontroller().getPageLanguageText());
-        mv.addObject("btcomp", iServiceIndex.getDcontroller().getBootstrapComponents());
 
         return mv;
     }
