e1b134bc0541c6ddadce013ce994f1d3226998b0
hoscho <jgeist@gmx.net>
2022-09-01 15:34:55 +0200

    .

--- src/main/java/de/jgsoftware/landingpage/iDemoLandingPage.java
+++ src/main/java/de/jgsoftware/landingpage/iDemoLandingPage.java
@@ -28,11 +28,10 @@ public interface iDemoLandingPage
         // 2. ZonedDateTime to TimeStamp , no different
         Timestamp timestamp2 = Timestamp.from(now.toInstant());
 
-        System.out.println(now);		// 2019-06-19T14:12:13.585294800+08:00[Asia/Kuala_Lumpur]
-
-        System.out.println(timestamp);	// 2019-06-19 14:12:13.5852948
-
+        System.out.println(now);
+        System.out.println(timestamp);
         System.out.println(timestamp2);
+
         SpringApplication.run(DemoLandingPageApplication.class, args);
     }
 }
