2c5ef7b919602efd7c1c5d8383b72ab16e27a81c
hoscho <jgeist@gmx.net>
2023-05-22 14:13:09 +0200

    edit start variable to starth2server

--- src/main/java/de/jgsoftware/landingpage/config/DemoDBConfig.java
+++ src/main/java/de/jgsoftware/landingpage/config/DemoDBConfig.java
@@ -1,7 +1,6 @@
 package de.jgsoftware.landingpage.config;
 
 
-import java.io.FileInputStream;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.io.File;
@@ -9,10 +8,8 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import com.zaxxer.hikari.HikariConfig;
-import org.h2.security.auth.ConfigProperties;
 import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
@@ -27,22 +24,6 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 @Configuration
 @EnableTransactionManagement
@@ -53,35 +34,14 @@ public class DemoDBConfig extends HikariConfig
 {
 
 
-    @Value("${startdb.rundb")
-    private Integer rundb;
 
-    public DemoDBConfig(@Value("${startdb.rundb}") Integer rundb)
+    public DemoDBConfig()
     {
-        /*
-            application.properties
-            startdb.rundb
-        */
-        this.rundb = rundb;
 
 
-        switch(rundb)
-        {
-            case 1:
-              startH2Server();
-               break;
-            case 2:
-              startderbydb();
-              break;
-            case 3:
-              startmysqldb();
-              break;
 
-            default:
-              startH2Server();
-              break;
-        }
 
+        startH2Server();
 
 
 
