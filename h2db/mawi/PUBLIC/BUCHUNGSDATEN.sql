7eb48ee884276dbaf6484781749bf1588e2cfa06
hoscho <jgeist@gmx.net>
2023-06-15 18:30:44 +0200

    edit folder to run dockerdb

--- h2db/mawi/PUBLIC/BUCHUNGSDATEN.sql
+++ h2db/mawi/PUBLIC/BUCHUNGSDATEN.sql
@@ -2,13 +2,13 @@ create table BUCHUNGSDATEN
 (
     ID            INT          not null
         primary key,
-    KDNUMMER      LONG         not null,
+    KDNUMMER      LONG VARCHAR         not null,
     KDNAME        VARCHAR(255) not null,
     BELEG         INT          not null,
     WG            INT          not null,
     VK            DOUBLE       not null,
     BDATUM        DATE         not null,
-    ARTIKELNUMMER LONG,
+    ARTIKELNUMMER LONG VARCHAR,
     BELEGART      INT,
     BUCHUNGSKZ    INT,
     MENGE         INT
