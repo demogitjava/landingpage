121a05ed610f16aaf8062c9144c4e3c10759931a
hoscho <jgeist@gmx.net>
2023-07-03 17:55:46 +0200

    edit main method to start application with graalvm17 without interface

--- src/main/java/de/jgsoftware/landingpage/DemoLandingPageApplication.java
+++ src/main/java/de/jgsoftware/landingpage/DemoLandingPageApplication.java
@@ -15,11 +15,17 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
 // de.jgsoftware.websitebuilder.DemoWebsitebuilderApplication
 @SpringBootApplication
-public class DemoLandingPageApplication implements iDemoLandingPage {
+public class DemoLandingPageApplication {
 
 
 
@@ -31,6 +37,53 @@ public class DemoLandingPageApplication implements iDemoLandingPage {
     //public static void main(String[] args) {
       //  SpringApplication.run(DemoLandingPageApplication.class, args);
    // }
+ public static String st_timezones = "Europe/Berlin";
+    public static String operationsytem = null;
 
+    public static void main(String[] args)
+    {
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of(st_timezones));
+        //ZonedDateTime now = ZonedDateTime.now();
+
+        // set up to GMT
+        now.withZoneSameInstant( ZoneId.of("UTC") )
+                .format( DateTimeFormatter.ISO_OFFSET_DATE_TIME );
+        // 1. ZonedDateTime to TimeStamp
+        Timestamp timestamp = Timestamp.valueOf(now.toLocalDateTime());
+
+        // 2. ZonedDateTime to TimeStamp , no different
+        Timestamp timestamp2 = Timestamp.from(now.toInstant());
+
+        System.out.println(now);
+        System.out.println(timestamp);
+        System.out.println(timestamp2);
+
+
+        String operationsytem = System.getProperty("os.name").toLowerCase();
+        if (operationsytem.contains("win")){
+            //Betriebssystem = Windows
+            operationsytem = "Windows";
+
+        }
+        else if (operationsytem.contains("osx")){
+            //Betriebssystem = OSX von Apple
+            operationsytem = "OSX Apple";
+        }
+        else if (operationsytem.contains("nix") || operationsytem.contains("aix") || operationsytem.contains("nux")){
+            //Betriebssystem = Unix bzw. Linux basiert
+            operationsytem = "unix-linux";
+        }
+        System.out.print(operationsytem + "\n");
+
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(DemoLandingPageApplication.class);
+        builder
+                // none
+                // reactive
+                // servlet
+                .web(WebApplicationType.SERVLET)
+               .headless(true).run();
+       // builder.run(args);
+        //SpringApplication.run(DemoLandingPageApplication.class, args);
+    }
 
 }
