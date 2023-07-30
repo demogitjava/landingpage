ba21e2388884bf0979a81bb2c71cf5e38100d2f1
hoscho <jgeist@gmx.net>
2023-07-30 12:05:00 +0200

    edit hibnateutil for connect to derby demodb

---
+++ src/main/java/de/jgsoftware/landingpage/config/HibernateUtildemodb.java
@@ -0,0 +1,54 @@
+
+package de.jgsoftware.landingpage.config;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ *
+ * @author hoscho
+ */
+
+public class HibernateUtildemodb {
+
+	private static SessionFactory sessionFactory;
+	
+	private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+        	Configuration configuration = new Configuration();
+        	configuration.configure("hibernate.cfg.demodb.xml");
+        	System.out.println("Hibernate Configuration loaded");
+        	
+        	ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        	System.out.println("Hibernate serviceRegistry created");
+        	
+        	SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        	
+            return sessionFactory;
+        }
+        catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            ex.printStackTrace();
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+	
+	public static SessionFactory getSessionFactory() {
+		if(sessionFactory == null) sessionFactory = buildSessionFactory();
+        return sessionFactory;
+    }
+        
+  
+    
+    public Session openSession()
+    {
+        Session session = HibernateUtildemodb.getSessionFactory().openSession();
+        session.beginTransaction();
+        return session;
+    }
+}
