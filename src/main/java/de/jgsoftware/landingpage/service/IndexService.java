3ad2654607710442b8787ba4eae9f8c4fca5b5e2
hoscho <jgeist@gmx.net>
2023-07-31 22:48:15 +0200

    edit IndexService

--- src/main/java/de/jgsoftware/landingpage/service/IndexService.java
+++ src/main/java/de/jgsoftware/landingpage/service/IndexService.java
@@ -5,7 +5,7 @@ import de.jgsoftware.landingpage.dao.Dao_DemoPageController;
 import de.jgsoftware.landingpage.service.interfaces.IServiceIndex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import de.jgsoftware.landingpage.dao.interfaces.web.IDaoDemoPageController;
+import de.jgsoftware.landingpage.dao.interfaces.shopdb.IDaoDemoPageController;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
