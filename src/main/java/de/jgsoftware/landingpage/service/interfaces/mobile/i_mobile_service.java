c89c94fea1faf8fe025b168108edf03f065bf03a
hoscho <jgeist@gmx.net>
2023-07-31 22:23:57 +0200

    edit mobile service

--- src/main/java/de/jgsoftware/landingpage/service/interfaces/mobile/i_mobile_service.java
+++ src/main/java/de/jgsoftware/landingpage/service/interfaces/mobile/i_mobile_service.java
@@ -1,7 +1,7 @@
 package de.jgsoftware.landingpage.service.interfaces.mobile;
 
 import de.jgsoftware.landingpage.dao.Dao_DemoPageController;
-import de.jgsoftware.landingpage.dao.interfaces.web.IDaoDemoPageController;
+import de.jgsoftware.landingpage.dao.interfaces.shopdb.IDaoDemoPageController;
 
 public interface i_mobile_service
 {
