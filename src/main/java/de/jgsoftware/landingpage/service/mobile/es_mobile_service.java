cf02551f05872162d400278443166eaec5fc2817
hoscho <jgeist@gmx.net>
2023-08-03 11:31:48 +0200

    add author to service

--- src/main/java/de/jgsoftware/landingpage/service/mobile/es_mobile_service.java
+++ src/main/java/de/jgsoftware/landingpage/service/mobile/es_mobile_service.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Service;
 import de.jgsoftware.landingpage.service.interfaces.mobile.i_es_mobile_service;
 import de.jgsoftware.landingpage.dao.interfaces.i_DAO_ES;
 
+/**
+ *
+ * @author hoscho
+ */
+
 @Service
 public class es_mobile_service implements i_es_mobile_service
 {
@@ -14,10 +19,12 @@ public class es_mobile_service implements i_es_mobile_service
     @Autowired
     i_DAO_ES i_dao_es;
 
+    @Override
     public i_DAO_ES getI_dao_es() {
         return i_dao_es;
     }
 
+    @Override
     public void setI_dao_es(i_DAO_ES i_dao_es) {
         this.i_dao_es = i_dao_es;
     }
