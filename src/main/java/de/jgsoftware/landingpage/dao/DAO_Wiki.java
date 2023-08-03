c15a0466384996f01763fc81b84c67ac7b633ab1
hoscho <jgeist@gmx.net>
2023-08-03 11:16:17 +0200

    add author

--- src/main/java/de/jgsoftware/landingpage/dao/DAO_Wiki.java
+++ src/main/java/de/jgsoftware/landingpage/dao/DAO_Wiki.java
@@ -4,6 +4,11 @@ package de.jgsoftware.landingpage.dao;
 import de.jgsoftware.landingpage.dao.interfaces.i_dao_wiki;
 import org.springframework.stereotype.Repository;
 
+
+/**
+ *
+ * @author hoscho
+ */
 @Repository
 public class DAO_Wiki implements i_dao_wiki
 {
