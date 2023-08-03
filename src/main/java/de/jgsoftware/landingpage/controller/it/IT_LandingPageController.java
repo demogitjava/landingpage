48c4ec5d03285f9c41d0c7295f0d283a40fa7e74
hoscho <jgeist@gmx.net>
2023-08-03 11:21:53 +0200

    add author

--- src/main/java/de/jgsoftware/landingpage/controller/it/IT_LandingPageController.java
+++ src/main/java/de/jgsoftware/landingpage/controller/it/IT_LandingPageController.java
@@ -7,6 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
+
+/**
+ *
+ * @author hoscho
+ */
+
 @Controller
 public class IT_LandingPageController implements iIT_Controller
 {
