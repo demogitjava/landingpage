43f89f050c5d7a4dd614819003cf5fccf51cf2a6
hoscho <jgeist@gmx.net>
2023-08-03 11:25:31 +0200

    add author

--- src/main/java/de/jgsoftware/landingpage/model/jpa/demodb/Users.java
+++ src/main/java/de/jgsoftware/landingpage/model/jpa/demodb/Users.java
@@ -3,6 +3,11 @@ package de.jgsoftware.landingpage.model.jpa.demodb;
 import javax.persistence.*;
 
 
+/**
+ *
+ * @author hoscho
+ */
+
 @Entity
 @Table(name = "Users", schema = "PUBLIC", catalog = "DEMODB")
 public class Users
