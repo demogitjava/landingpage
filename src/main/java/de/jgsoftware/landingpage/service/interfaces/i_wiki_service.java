a6c66087b5179a03806be1e4bb0cd65fd6cd7492
hoscho <jgeist@gmx.net>
2023-08-03 11:30:19 +0200

    add author to interface

--- src/main/java/de/jgsoftware/landingpage/service/interfaces/i_wiki_service.java
+++ src/main/java/de/jgsoftware/landingpage/service/interfaces/i_wiki_service.java
@@ -2,6 +2,11 @@ package de.jgsoftware.landingpage.service.interfaces;
 
 import de.jgsoftware.landingpage.dao.interfaces.i_dao_wiki;
 
+
+/**
+ *
+ * @author hoscho
+ */
 public interface i_wiki_service
 {
     i_dao_wiki getiDaoWiki();
