cedfa5fe924e8dfcb02d110e3c5e452a7deb0aae
hoscho <jgeist@gmx.net>
2023-08-03 11:29:17 +0200

    add author to interface

--- src/main/java/de/jgsoftware/landingpage/service/interfaces/mobile/i_fr_mobile_service.java
+++ src/main/java/de/jgsoftware/landingpage/service/interfaces/mobile/i_fr_mobile_service.java
@@ -3,6 +3,11 @@ package de.jgsoftware.landingpage.service.interfaces.mobile;
 import de.jgsoftware.landingpage.dao.interfaces.i_DAO_ES;
 import de.jgsoftware.landingpage.dao.interfaces.i_DAO_FR;
 
+/**
+ *
+ * @author hoscho
+ */
+
 public interface i_fr_mobile_service
 {
     i_DAO_FR getI_dao_fr();
