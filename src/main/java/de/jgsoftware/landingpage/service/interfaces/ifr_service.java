9fc0af0d8ed894551c71794b31fbebfff627dbfa
hoscho <jgeist@gmx.net>
2023-08-03 11:30:42 +0200

    add author to interface

--- src/main/java/de/jgsoftware/landingpage/service/interfaces/ifr_service.java
+++ src/main/java/de/jgsoftware/landingpage/service/interfaces/ifr_service.java
@@ -2,6 +2,11 @@ package de.jgsoftware.landingpage.service.interfaces;
 
 import de.jgsoftware.landingpage.dao.interfaces.i_DAO_FR;
 
+
+/**
+ *
+ * @author hoscho
+ */
 public interface ifr_service
 {
     i_DAO_FR getI_dao_fr();
