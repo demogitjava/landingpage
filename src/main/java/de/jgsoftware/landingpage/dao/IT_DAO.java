c7359d32c2464016bba8ca8aacf1109f6102a9dd
hoscho <jgeist@gmx.net>
2023-08-03 11:55:11 +0200

    edit mehtod
      public List<m_webtextlayout> getPageLanguageText()
    
    load list over jpa interface

--- src/main/java/de/jgsoftware/landingpage/dao/IT_DAO.java
+++ src/main/java/de/jgsoftware/landingpage/dao/IT_DAO.java
@@ -1,6 +1,8 @@
 package de.jgsoftware.landingpage.dao;
 
 import de.jgsoftware.landingpage.dao.interfaces.i_DAO_IT;
+import de.jgsoftware.landingpage.dao.interfaces.shopdb.i_jpa_bootstrap_components;
+import de.jgsoftware.landingpage.dao.interfaces.shopdb.i_jpa_webtextlayout;
 import de.jgsoftware.landingpage.model.m_bootstrap_components;
 import de.jgsoftware.landingpage.model.m_webtextlayout;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,14 +26,28 @@ public class IT_DAO implements i_DAO_IT
     @Autowired
     @Qualifier(value = "shopJdbcTemplate")
     JdbcTemplate jtm2;
+    
+    
+      
+    @Autowired
+    i_jpa_webtextlayout ijpashopwebtextlayout;
+    
+    @Autowired
+    i_jpa_bootstrap_components ijpashopbootstrapcomponents;
+
+    
 
 
     // returns all entriys from Table
     @Override
     public List<m_webtextlayout> getPageLanguageText()
     {
-        List<m_webtextlayout> webtextlayouts = jtm2.query("select * from webtextlayout", new BeanPropertyRowMapper(m_webtextlayout.class));
+        
+        //manuel Query jdbcTempalte jtm2 shopdb
+        //List<m_webtextlayout> webtextlayouts = jtm2.query("select * from webtextlayout", new BeanPropertyRowMapper(m_webtextlayout.class));
 
+        List<m_webtextlayout> webtextlayouts = (List) ijpashopwebtextlayout.findAll();
+        
         return webtextlayouts;
     }
 
