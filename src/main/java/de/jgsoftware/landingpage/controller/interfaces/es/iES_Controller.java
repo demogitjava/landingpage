baba449f4c6ab0849f2798c4fba217ba6c573770
hoscho <jgeist@gmx.net>
2023-08-03 11:19:23 +0200

    add author to interface

--- src/main/java/de/jgsoftware/landingpage/controller/interfaces/es/iES_Controller.java
+++ src/main/java/de/jgsoftware/landingpage/controller/interfaces/es/iES_Controller.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
+/**
+ *
+ * @author hoscho
+ */
 @RequestMapping("/es")
 public interface iES_Controller
 {
