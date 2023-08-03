f2e6881e201d8727486fb47e4b386d39af9793d2
hoscho <jgeist@gmx.net>
2023-08-03 11:21:41 +0200

    add author to interface

--- src/main/java/de/jgsoftware/landingpage/controller/interfaces/i_de_wiki.java
+++ src/main/java/de/jgsoftware/landingpage/controller/interfaces/i_de_wiki.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ *
+ * @author hoscho
+ */
+
 @RequestMapping("/wiki")
 public interface i_de_wiki
 {
