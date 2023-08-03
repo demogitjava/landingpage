ba816e61e9175dc202a467da08d5807b017ba61b
hoscho <jgeist@gmx.net>
2023-08-03 11:32:26 +0200

    add author to service

--- src/main/java/de/jgsoftware/landingpage/service/mobile/tr_mobile_service.java
+++ src/main/java/de/jgsoftware/landingpage/service/mobile/tr_mobile_service.java
@@ -6,6 +6,11 @@ import de.jgsoftware.landingpage.service.interfaces.mobile.i_tr_mobile_service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ *
+ * @author hoscho
+ */
+
 @Service
 public class tr_mobile_service implements i_tr_mobile_service
 {
@@ -14,10 +19,12 @@ public class tr_mobile_service implements i_tr_mobile_service
     @Autowired
     i_DAO_TR i_dao_tr;
 
+    @Override
     public i_DAO_TR getI_dao_tr() {
         return i_dao_tr;
     }
 
+    @Override
     public void setI_dao_tr(i_DAO_TR i_dao_tr) {
         this.i_dao_tr = i_dao_tr;
     }
