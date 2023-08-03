0d090243c414f192000d3789a6178cbbfb743eaf
hoscho <jgeist@gmx.net>
2023-08-03 11:19:16 +0200

    add author to interface

--- src/main/java/de/jgsoftware/landingpage/controller/interfaces/en/iEN_Controller.java
+++ src/main/java/de/jgsoftware/landingpage/controller/interfaces/en/iEN_Controller.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
+/**
+ *
+ * @author hoscho
+ */
 @RequestMapping("/en")
 public interface iEN_Controller
 {
