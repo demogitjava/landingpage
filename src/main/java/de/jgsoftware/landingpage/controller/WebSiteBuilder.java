a02390cf83e7b4d3043a1f1de12de7b01a8e0eed
hoscho <jgeist@gmx.net>
2023-08-03 11:18:25 +0200

    add author

--- src/main/java/de/jgsoftware/landingpage/controller/WebSiteBuilder.java
+++ src/main/java/de/jgsoftware/landingpage/controller/WebSiteBuilder.java
@@ -8,6 +8,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ *
+ * @author hoscho
+ */
+
+
 @Controller
 public class WebSiteBuilder implements iWebSiteBuilder
 {
