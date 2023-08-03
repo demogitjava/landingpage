178ccdeb60daf9be2bbe9b900b932481b5ac175f
hoscho <jgeist@gmx.net>
2023-08-03 11:34:32 +0200

    add author

--- src/main/java/de/jgsoftware/landingpage/service/it_service.java
+++ src/main/java/de/jgsoftware/landingpage/service/it_service.java
@@ -6,16 +6,25 @@ import org.springframework.stereotype.Service;
 
 import de.jgsoftware.landingpage.dao.interfaces.i_DAO_IT;
 
+
+
+/**
+ *
+ * @author hoscho
+ */
+
 @Service
 public class it_service implements iit_service
 {
     @Autowired
     i_DAO_IT i_dao_it;
 
+    @Override
     public i_DAO_IT getI_dao_it() {
         return i_dao_it;
     }
-
+    
+    @Override
     public void setI_dao_it(i_DAO_IT i_dao_it) {
         this.i_dao_it = i_dao_it;
     }
