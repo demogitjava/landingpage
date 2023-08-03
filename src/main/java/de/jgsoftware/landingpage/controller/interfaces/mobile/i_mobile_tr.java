1a523e253031c9c82f457d86d4dc98b1657ab145
hoscho <jgeist@gmx.net>
2023-08-03 11:20:25 +0200

    add author to interface

--- src/main/java/de/jgsoftware/landingpage/controller/interfaces/mobile/i_mobile_tr.java
+++ src/main/java/de/jgsoftware/landingpage/controller/interfaces/mobile/i_mobile_tr.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ *
+ * @author hoscho
+ */
+
 @RequestMapping("/m_tr")
 public interface i_mobile_tr {
 
