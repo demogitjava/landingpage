106224629810e63c32745a9efc8b1bc0aaa65db2
hoscho <jgeist@gmx.net>
2023-08-03 11:14:19 +0200

    add author to interface

--- src/main/java/de/jgsoftware/landingpage/dao/interfaces/demodb/Int_m_webtextlayout.java
+++ src/main/java/de/jgsoftware/landingpage/dao/interfaces/demodb/Int_m_webtextlayout.java
@@ -3,6 +3,11 @@ package de.jgsoftware.landingpage.dao.interfaces.demodb;
 import de.jgsoftware.landingpage.model.m_webtextlayout;
 import org.springframework.data.repository.CrudRepository;
 
+
+/**
+ *
+ * @author hoscho
+ */
 public interface Int_m_webtextlayout extends CrudRepository<m_webtextlayout, Integer>
 {
 
