ffa6fa48e865a61be381be86ac61aa89e2679019
hoscho <jgeist@gmx.net>
2023-08-03 11:20:41 +0200

    add author to interface

--- src/main/java/de/jgsoftware/landingpage/controller/interfaces/Int_m_webtextlayout.java
+++ src/main/java/de/jgsoftware/landingpage/controller/interfaces/Int_m_webtextlayout.java
@@ -4,6 +4,11 @@ package de.jgsoftware.landingpage.controller.interfaces;
 import de.jgsoftware.landingpage.model.m_webtextlayout;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ *
+ * @author hoscho
+ */
+
 public interface Int_m_webtextlayout extends CrudRepository<m_webtextlayout, Integer>
 {
 
