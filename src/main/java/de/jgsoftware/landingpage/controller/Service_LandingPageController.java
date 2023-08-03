f1b729ef571d7f06ea55a06c23639e3477e4fa11
hoscho <jgeist@gmx.net>
2023-08-03 11:18:17 +0200

    add author

--- src/main/java/de/jgsoftware/landingpage/controller/Service_LandingPageController.java
+++ src/main/java/de/jgsoftware/landingpage/controller/Service_LandingPageController.java
@@ -5,6 +5,11 @@ import de.jgsoftware.landingpage.controller.interfaces.iServiceController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ *
+ * @author hoscho
+ */
+
 @Controller
 public class Service_LandingPageController implements iServiceController
 {
