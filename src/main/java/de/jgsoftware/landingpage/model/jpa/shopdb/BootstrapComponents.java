3e9d9fed8bf2ca7e185a3cb9beff7f067fffe473
hoscho <jgeist@gmx.net>
2023-08-03 11:26:33 +0200

    add author

--- src/main/java/de/jgsoftware/landingpage/model/jpa/shopdb/BootstrapComponents.java
+++ src/main/java/de/jgsoftware/landingpage/model/jpa/shopdb/BootstrapComponents.java
@@ -2,6 +2,11 @@ package de.jgsoftware.landingpage.model.jpa.shopdb;
 
 import javax.persistence.*;
 
+/**
+ *
+ * @author hoscho
+ */
+
 @Entity
 @Table(name = "BOOTSTRAP_COMPONENTS", schema = "PUBLIC", catalog = "SHOPDB")
 public class BootstrapComponents {
