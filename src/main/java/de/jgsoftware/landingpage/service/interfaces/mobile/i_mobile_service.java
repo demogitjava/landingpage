4bc60f8013d161550e218e00e798bdd63fc6e220
hoscho <jgeist@gmx.net>
2023-08-03 11:29:32 +0200

    add author to interface

--- src/main/java/de/jgsoftware/landingpage/service/interfaces/mobile/i_mobile_service.java
+++ src/main/java/de/jgsoftware/landingpage/service/interfaces/mobile/i_mobile_service.java
@@ -3,6 +3,11 @@ package de.jgsoftware.landingpage.service.interfaces.mobile;
 import de.jgsoftware.landingpage.dao.Dao_DemoPageController;
 import de.jgsoftware.landingpage.dao.interfaces.shopdb.IDaoDemoPageController;
 
+/**
+ *
+ * @author hoscho
+ */
+
 public interface i_mobile_service
 {
     IDaoDemoPageController getDcontroller();
