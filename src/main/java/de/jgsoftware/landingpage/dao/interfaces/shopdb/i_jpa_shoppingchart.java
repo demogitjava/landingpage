388ec527cd585ce731d09affb615c73a81de8686
hoscho <jgeist@gmx.net>
2023-08-08 18:11:15 +0200

    add jpa_interface
    i_jpa_shoppingchart

---
+++ src/main/java/de/jgsoftware/landingpage/dao/interfaces/shopdb/i_jpa_shoppingchart.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package de.jgsoftware.landingpage.dao.interfaces.shopdb;
+
+import de.jgsoftware.landingpage.model.jpa.shopdb.Shoppingcart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author hoscho
+ */
+
+@Repository
+public interface i_jpa_shoppingchart extends JpaRepository<Shoppingcart, Integer>
+{
+}
