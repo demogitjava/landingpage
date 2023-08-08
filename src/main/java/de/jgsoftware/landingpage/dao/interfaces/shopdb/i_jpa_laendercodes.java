436698c326dfa02531f3c83b5c0655091f14d48c
hoscho <jgeist@gmx.net>
2023-08-08 18:08:40 +0200

    .

--- src/main/java/de/jgsoftware/landingpage/dao/interfaces/shopdb/i_jpa_laendercodes.java
+++ src/main/java/de/jgsoftware/landingpage/dao/interfaces/shopdb/i_jpa_laendercodes.java
@@ -5,7 +5,6 @@
 package de.jgsoftware.landingpage.dao.interfaces.shopdb;
 
 import de.jgsoftware.landingpage.model.jpa.shopdb.Laendercodes;
-import de.jgsoftware.landingpage.model.useragent;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
