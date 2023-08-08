b6a10a1182912bd0254a97c12722a5ac2036344c
hoscho <jgeist@gmx.net>
2023-08-08 17:41:25 +0200

    add jpa interface
    i_jpa_userdetail

---
+++ src/main/java/de/jgsoftware/landingpage/dao/interfaces/demodb/i_jpa_userdetail.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package de.jgsoftware.landingpage.dao.interfaces.demodb;
+
+import de.jgsoftware.landingpage.model.jpa.demodb.Userdata;
+import de.jgsoftware.landingpage.model.jpa.demodb.Userdetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author hoscho
+ */
+@Repository
+public interface i_jpa_userdetail extends JpaRepository<Userdetail, Integer>
+{
+    
+}
