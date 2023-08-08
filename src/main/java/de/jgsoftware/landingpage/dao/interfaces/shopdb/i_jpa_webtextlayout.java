b830461fa7e60cde69cc5f6cf4306998f24d5eb6
hoscho <jgeist@gmx.net>
2023-08-08 18:17:58 +0200

    .

--- src/main/java/de/jgsoftware/landingpage/dao/interfaces/shopdb/i_jpa_webtextlayout.java
+++ src/main/java/de/jgsoftware/landingpage/dao/interfaces/shopdb/i_jpa_webtextlayout.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package de.jgsoftware.landingpage.dao.interfaces.shopdb;
 
 import de.jgsoftware.landingpage.model.m_webtextlayout;
