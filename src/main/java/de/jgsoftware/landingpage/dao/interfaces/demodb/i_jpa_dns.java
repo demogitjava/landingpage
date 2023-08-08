52ed2d347f0b6e6f97a3a3a52fe0744b7ac545f9
hoscho <jgeist@gmx.net>
2023-08-08 17:25:30 +0200

    add jpa interface for dns table on demodb

---
+++ src/main/java/de/jgsoftware/landingpage/dao/interfaces/demodb/i_jpa_dns.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package de.jgsoftware.landingpage.dao.interfaces.demodb;
+
+import de.jgsoftware.landingpage.model.jpa.demodb.Dns;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author hoscho
+ */
+@Repository
+public interface i_jpa_dns extends JpaRepository<Dns, Integer>{
+    
+}
