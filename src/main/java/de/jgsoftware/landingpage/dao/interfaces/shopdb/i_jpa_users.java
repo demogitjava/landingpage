ee25fb7bbe88ca41c7cce171335c9835d32d83fa
hoscho <jgeist@gmx.net>
2023-08-08 18:13:37 +0200

    add jpa_interface
    i_jpa_users

---
+++ src/main/java/de/jgsoftware/landingpage/dao/interfaces/shopdb/i_jpa_users.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package de.jgsoftware.landingpage.dao.interfaces.shopdb;
+
+
+import de.jgsoftware.landingpage.model.Users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author hoscho
+ */
+
+@Repository
+public interface i_jpa_users extends JpaRepository<Users, Integer>
+{
+}
