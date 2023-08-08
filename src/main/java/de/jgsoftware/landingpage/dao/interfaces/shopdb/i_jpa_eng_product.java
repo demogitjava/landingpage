ed9eac72e513c8cea8acd9c6f7b082b4932e71ef
hoscho <jgeist@gmx.net>
2023-08-08 18:04:38 +0200

    add @Repository to jpa interface

--- src/main/java/de/jgsoftware/landingpage/dao/interfaces/shopdb/i_jpa_eng_product.java
+++ src/main/java/de/jgsoftware/landingpage/dao/interfaces/shopdb/i_jpa_eng_product.java
@@ -2,11 +2,13 @@ package de.jgsoftware.landingpage.dao.interfaces.shopdb;
 
 import de.jgsoftware.landingpage.model.jpa.shopdb.EngProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author hoscho
  */
+@Repository
 public interface i_jpa_eng_product extends JpaRepository<EngProduct, Integer>
 {
     
