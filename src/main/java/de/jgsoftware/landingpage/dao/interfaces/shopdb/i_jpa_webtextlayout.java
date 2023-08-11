9832541f35a5e63ed94971b44c8ec2a3fb376a6b
hoscho <jgeist@gmx.net>
2023-08-11 21:27:11 +0200

    edit i_jpa_webtextlayout
    edit import path to 
    import de.jgsoftware.landingpage.model.jpa.shopdb.Webtextlayout;

--- src/main/java/de/jgsoftware/landingpage/dao/interfaces/shopdb/i_jpa_webtextlayout.java
+++ src/main/java/de/jgsoftware/landingpage/dao/interfaces/shopdb/i_jpa_webtextlayout.java
@@ -1,6 +1,6 @@
 package de.jgsoftware.landingpage.dao.interfaces.shopdb;
 
-import de.jgsoftware.landingpage.model.m_webtextlayout;
+import de.jgsoftware.landingpage.model.jpa.shopdb.Webtextlayout;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
  * @author hoscho
  */
 @Repository
-public interface i_jpa_webtextlayout extends JpaRepository<m_webtextlayout, Integer>
+public interface i_jpa_webtextlayout extends JpaRepository<Webtextlayout, Integer>
 {
     
 }
