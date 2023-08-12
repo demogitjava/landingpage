de7f06c5932d40527ee33b5973c679555ebe71ff
hoscho <jgeist@gmx.net>
2023-08-12 10:10:09 +0200

    edit i_jpa_useragent
    import model demodb Useragent

--- src/main/java/de/jgsoftware/landingpage/dao/interfaces/demodb/j_jpa_useragent.java
+++ src/main/java/de/jgsoftware/landingpage/dao/interfaces/demodb/j_jpa_useragent.java
@@ -20,11 +20,11 @@ public interface j_jpa_useragent extends JpaRepository<Useragent, Integer>
     
     // derbydb sql   SELECT COUNT(ipaddress) as count, MONTH(datum) as monat, YEAR(datum) as jahr FROM useragent a GROUP BY YEAR(datum), MONTH(datum)
     // SELECT d.name, COUNT(e) FROM Department d JOIN d.employees e GROUP BY d.name
-    //@Query("select count(u.id) as count, YEAR(u.datum) as jahr, MONTH(u.datum) as monat from useragent u group by YEAR(u.datum), MONTH(u.datum)")
-    //List<useragent> useragentgroupbyyearmonth();
+    @Query("select count(u.id) as count, YEAR(u.datum) as jahr, MONTH(u.datum) as monat from useragent u group by YEAR(u.datum), MONTH(u.datum)")
+    List<Useragent> useragentgroupbyyearmonth();
     
-    //@Query("select count(u.id) as count, YEAR(u.datum) as jahr, MONTH(u.datum) as monat from useragent u group by YEAR(u.datum), MONTH(u.datum)")
-    //List<useragent> useragentcountmonth();
+    @Query("select count(u.id) as count, YEAR(u.datum) as jahr, MONTH(u.datum) as monat from useragent u group by YEAR(u.datum), MONTH(u.datum)")
+    List<Useragent> useragentcountmonth();
     
  
     
