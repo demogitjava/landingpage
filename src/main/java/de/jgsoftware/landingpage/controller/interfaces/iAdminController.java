906c488965af1f805b288997c615583e1c16333b
hoscho <jgeist@gmx.net>
2023-08-28 12:42:56 +0200

    edit interface iAdminController
    Webtextcomponent

--- src/main/java/de/jgsoftware/landingpage/controller/interfaces/iAdminController.java
+++ src/main/java/de/jgsoftware/landingpage/controller/interfaces/iAdminController.java
@@ -1,7 +1,7 @@
 package de.jgsoftware.landingpage.controller.interfaces;
 
 import de.jgsoftware.landingpage.model.jpa.demodb.Webtextlayout;
-import de.jgsoftware.landingpage.model.jpa.shopdb.m_webtextlayout;
+//import de.jgsoftware.landingpage.model.jpa.shopdb.m_webtextlayout;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
