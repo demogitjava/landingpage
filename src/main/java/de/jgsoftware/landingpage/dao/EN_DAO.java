cc7c476886d75732b53a18aa5bd97b025bac3c15
hoscho <jgeist@gmx.net>
2023-08-28 12:41:11 +0200

    eidt EN_Dao

--- src/main/java/de/jgsoftware/landingpage/dao/EN_DAO.java
+++ src/main/java/de/jgsoftware/landingpage/dao/EN_DAO.java
@@ -4,8 +4,12 @@ package de.jgsoftware.landingpage.dao;
 import de.jgsoftware.landingpage.dao.interfaces.i_DAO_EN;
 import de.jgsoftware.landingpage.dao.interfaces.shopdb.i_jpa_bootstrap_components;
 import de.jgsoftware.landingpage.dao.interfaces.shopdb.i_jpa_webtextlayout;
-import de.jgsoftware.landingpage.model.m_bootstrap_components;
-import de.jgsoftware.landingpage.model.m_webtextlayout;
+
+import de.jgsoftware.landingpage.model.jpa.shopdb.BootstrapComponents;
+//import de.jgsoftware.landingpage.model.m_bootstrap_components;
+
+
+import de.jgsoftware.landingpage.model.jpa.shopdb.Webtextlayout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -41,13 +45,13 @@ public class EN_DAO implements i_DAO_EN
 
     // returns all entriys from Table
     @Override
-    public List<m_webtextlayout> getPageLanguageText()
+    public List<Webtextlayout> getPageLanguageText()
     {
         
         // jdbc Tempalte jtm2 shop manuel query
         //List<m_webtextlayout> webtextlayouts = jtm2.query("select * from webtextlayout", new BeanPropertyRowMapper(m_webtextlayout.class));
 
-        List<m_webtextlayout> webtextlayouts = (List) ijpashopwebtextlayout.findAll();
+        List<Webtextlayout> webtextlayouts = (List) ijpashopwebtextlayout.findAll();
           
         return webtextlayouts;
     }
@@ -55,8 +59,13 @@ public class EN_DAO implements i_DAO_EN
 
     // returns all entriys from stored Bootstrap Compoents
     // with ${ value } for mapping to Theamyleaf
+
+    /**
+     *
+     * @return
+     */
     @Override
-    public List<m_bootstrap_components> getBootstrapComponents()
+    public List<BootstrapComponents> getBootstrapComponents()
     {
 
         // query("select * from bootstrap_components", new BeanPropertyRowMapper(m_bootstrap_comonents.class));
@@ -67,7 +76,7 @@ public class EN_DAO implements i_DAO_EN
         //List<m_bootstrap_components> bootstrap_comonents = jtm2.query("select * from BOOTSTRAP_COMPONENTS", new BeanPropertyRowMapper(m_bootstrap_components.class));
 
         // ijpashopbootstrapcomponents
-        List<m_bootstrap_components> bootstrap_comonents = (List) ijpashopbootstrapcomponents.findAll();
+        List<BootstrapComponents> bootstrap_comonents = (List) ijpashopbootstrapcomponents.findAll();
 
         return bootstrap_comonents;
     }
