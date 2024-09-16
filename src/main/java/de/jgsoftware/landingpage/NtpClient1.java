dcd77611632572ffa3c2aa5d1c6220930dc7a236
hoscho <jgeist@gmx.net>
2024-09-16 11:50:35 +0200

    edit NtpClient function for systemtime

--- src/main/java/de/jgsoftware/landingpage/NtpClient1.java
+++ src/main/java/de/jgsoftware/landingpage/NtpClient1.java
@@ -4,7 +4,6 @@ package de.jgsoftware.landingpage;
 
 
 import java.net.InetAddress;
-import java.util.Date;
 import org.apache.commons.net.ntp.NTPUDPClient; 
 import org.apache.commons.net.ntp.TimeInfo;
 import org.apache.commons.net.ntp.TimeStamp;
@@ -16,18 +15,20 @@ import org.apache.commons.net.ntp.TimeStamp;
  * @author hoscho
  */
 public class NtpClient1 {
-  private static final String SERVER_NAME = "0.suse.pool.ntp.org";
+  private static final String NTPSERVER_NAME = "0.suse.pool.ntp.org";
 
   private volatile TimeInfo timeInfo;
   private volatile Long offset;
 
-  public NtpClient1() throws Exception {
+  public NtpClient1() {
 
-    NTPUDPClient client = new NTPUDPClient();
+    try
+{
+        NTPUDPClient client = new NTPUDPClient();
     // We want to timeout if a response takes longer than 10 seconds
     client.setDefaultTimeout(10_000);
 
-    InetAddress inetAddress = InetAddress.getByName(SERVER_NAME);
+    InetAddress inetAddress = InetAddress.getByName(NTPSERVER_NAME);
     TimeInfo timeInfo = client.getTime(inetAddress);
     timeInfo.computeDetails();
     if (timeInfo.getOffset() != null) {
@@ -37,13 +38,27 @@ public class NtpClient1 {
 
     // This system NTP time
     TimeStamp systemNtpTime = TimeStamp.getCurrentTime();
-    System.out.println("System time:\t" + systemNtpTime + "  " + systemNtpTime.toDateString());
-
+    System.out.println("System time:" + "\t" +systemNtpTime + "  " + systemNtpTime.toDateString());
+ } catch(Exception e)
+    {
+        System.out.print("Fehler " + e);
+    }
    
   }
 
   public boolean isComputed()
   {
     return timeInfo != null && offset != null;
+  }
+
+
+  /*
+
+     return the ntp server name
+  */
+  public String getNtpServername()
+  {
+
+     return NTPSERVER_NAME;
   }
 }
--- src/main/java/de/jgsoftware/landingpage/NtpClient3.java
+++ src/main/java/de/jgsoftware/landingpage/NtpClient3.java
@@ -4,7 +4,6 @@ package de.jgsoftware.landingpage;
 
 
 import java.net.InetAddress;
-import java.util.Date;
 import org.apache.commons.net.ntp.NTPUDPClient; 
 import org.apache.commons.net.ntp.TimeInfo;
 import org.apache.commons.net.ntp.TimeStamp;
@@ -16,18 +15,20 @@ import org.apache.commons.net.ntp.TimeStamp;
  * @author hoscho
  */
 public class NtpClient3 {
-  private static final String SERVER_NAME = "2.suse.pool.ntp.org";
+  public static final String NTPSERVER_NAME = "2.suse.pool.ntp.org";
 
   private volatile TimeInfo timeInfo;
   private volatile Long offset;
 
-  public NtpClient3() throws Exception {
+  public NtpClient3() {
 
-    NTPUDPClient client = new NTPUDPClient();
+   try
+{
+        NTPUDPClient client = new NTPUDPClient();
     // We want to timeout if a response takes longer than 10 seconds
     client.setDefaultTimeout(10_000);
 
-    InetAddress inetAddress = InetAddress.getByName(SERVER_NAME);
+    InetAddress inetAddress = InetAddress.getByName(NTPSERVER_NAME);
     TimeInfo timeInfo = client.getTime(inetAddress);
     timeInfo.computeDetails();
     if (timeInfo.getOffset() != null) {
@@ -37,13 +38,26 @@ public class NtpClient3 {
 
     // This system NTP time
     TimeStamp systemNtpTime = TimeStamp.getCurrentTime();
-    System.out.println("System time:\t" + systemNtpTime + "  " + systemNtpTime.toDateString());
-
+    System.out.println("System time:" + "\t" +systemNtpTime + "  " + systemNtpTime.toDateString());
+ } catch(Exception e)
+    {
+        System.out.print("Fehler " + e);
+    }
    
   }
 
   public boolean isComputed()
   {
     return timeInfo != null && offset != null;
+  }
+
+/*
+
+     return the ntp server name
+  */
+  public String getNtpServername()
+  {
+
+     return NTPSERVER_NAME;
   }
 }
